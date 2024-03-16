27c6aa5669e9249f69d32d9ad586fdfd6a14c8fa
MTACreations <mohammad.almheiri@student.saedubai.com>
2024-03-16 22:32:49 +0400

    New cards + Timer is now working

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public float time = 30;
+    public Text timeText;
+
     public SceneLoader sceneLoader;
 
     public void Update()
     {
         time -= Time.deltaTime;
+        Countdown(time);
 
         if (time < 0)
         {
@@ -25,5 +29,15 @@ public class Timer : MonoBehaviour
     public void LoseTime(int penaltyTime)
     {
         time = time - penaltyTime;
+    }
+
+    public void Countdown(float timeLeft)
+    {
+        timeLeft += 1;
+
+        float minutesLeft = Mathf.FloorToInt(timeLeft / 60);
+        float secondsLeft = Mathf.FloorToInt(timeLeft % 60);
+
+        timeText.text = string.Format("{0:00} : {1:00}", minutesLeft, secondsLeft);
     }
 }
